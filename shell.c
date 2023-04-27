06d061a54d1a7624210fdcace1dcaa348a8c17b2
chriss1525 <okothchris15@gmail.com>
2023-04-27 22:51:30 +0300

    remove all code (handling newline)

--- shell.c
+++ shell.c
@@ -10,92 +10,99 @@
  * Return: 0 (success)
  */
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 20
 
 int main(void)
 {
-	char input[BUFFER_SIZE];
-	char **args;
-	char *newline;
-	char *token;
-	int i;
-	pid_t pid;
+    char input[BUFFER_SIZE];
+    char **args;
+    /*char *newline;*/
+    char *token;
+    int i;
+    pid_t pid;
 
-	while (1)
-	{
-		/* Display prompt in parent process */
-		if (getpid() == getpgrp())
-			_printf("> ");
+    while (1)
+    {
+        /* Display prompt in parent process */
+        if (getpid() == getpgrp()) {
+            write(STDOUT_FILENO, "> ", 2);
+            fflush(stdout);
+        }
 
-		/* Wait for user input */
-		if (fgets(input, BUFFER_SIZE, stdin) == NULL)
-		{
-			_printf("\n");
-			exit(0);
-		}
+        /* Wait for user input */
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL)
+        {
+            write(STDOUT_FILENO, "\n", 2);
+            exit(0);
+        }
 
-		/* Check for EOF (Ctrl+D) */
-		if (feof(stdin))
-		{
-			_printf("\n");
-			exit(0);
-		}
+        /* Check for EOF (Ctrl+D) */
+        if (feof(stdin))
+        {
+            write(STDOUT_FILENO, "\n", 2);
+            exit(0);
+        }
 
-		/* Strip newline character */
-		newline = strchr(input, '\n');
-		if (newline != NULL)
-		{
-			*newline = '\0';
-		}
+        /* Strip newline character 
+        newline = strchr(input, '\n');
+        if (newline != NULL)
+        {
+            *newline = '\0';
+        }*/
 
-		/* Remove trailing newline */
-		input[strcspn(input, "\n")] = '\0';
+        /* Remove trailing newline */
+        /*input[strcspn(input, "\n")] = '\0';*/
 
-		/* Trim leading/trailing whitespace */
-		token = strtok(input, " \n\t\r");
 
-		/* Tokenize input into arguments */
-		args = malloc(sizeof(char *));
-		i = 0;
-		while (token != NULL)
-		{
-			args = realloc(args, sizeof(char *) * (i + 2));
-			args[i] = token;
-			token = strtok(NULL, " \n\t\r");
-			i++;
-		}
-		args[i] = NULL;
+        /* Tokenize input into arguments */
+        token = strtok(input, " \n\t\r");
 
-		/* Execute command */
-		if (args[0] != NULL)
-		{
-			pid = fork();
+        args = malloc(sizeof(char *));
+        i = 0;
+        while (token != NULL)
+        {
+            args = realloc(args, sizeof(char *) * (i + 2));
+            args[i] = token;
+            token = strtok(NULL, " \n\t\r");
+            i++;
+        }
+        args[i] = NULL;
 
-			if (pid == -1)
-			{
-				perror("Error: fork failed");
-				exit(1);
-			}
-			else if (pid == 0)
-			{
-				/* Child process */
-				if (execvp(args[0], args) == -1)
-				{
-					_printf("Error: Command not found.\n");
-					exit(1);
-				}
-			}
-			else
-			{
-				/* Parent process */
-				int status;
-				waitpid(pid, &status, 0);
-			}
+        /* Execute command */
+        if (args[0] != NULL)
+        {
+            pid = fork();
 
-			free(args);
-			args = NULL;
-		}
-	}
+            if (pid == -1)
+            {
+                perror("Error: fork failed");
+                exit(1);
+            }
+            else if (pid == 0)
+            {
+                /* Child process */
+                if (execvp(args[0], args) == -1)
+                {
+                    _printf("Error: Command not found.\n");
+                    exit(1);
+                }
+            }
+            else
+            {
+                /* Parent process */
+                int status;
+                waitpid(pid, &status, 0);
+            }
 
-	return 0;
+            free(args);
+            /*args = NULL;*/
+        }
+        
+        /* Check if last character of input is newline character */
+        /*if (input[strlen(input) - 1] == '\n') {
+            _printf("\b");
+        }*/
+    }
+
+    return 0;
 }
